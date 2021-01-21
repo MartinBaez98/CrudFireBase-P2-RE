fd2032ef2c5d80952c62b4b6567f046a2d501a5f
Baez <maallopezba@ittepic.edu.mx>
2021-01-21 08:35:08 -0700

    List of active and inactive students separately

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,11 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  }
+  },
+  {
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+  },
 ];
 @NgModule({
   imports: [
